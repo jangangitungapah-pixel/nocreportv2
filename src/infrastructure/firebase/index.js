3c9bff6e43afe6cfdff0545ab4f852a82608ae88
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-21 04:22:17 +0700

    chore(t5): expose Firebase infrastructure boundary

---
+++ src/infrastructure/firebase/index.js
@@ -0,0 +1,14 @@
+export { getAuthClient } from './authClient.js';
+export {
+  getFirebaseConfigStatus,
+  readEmulatorConfig,
+  readFirebaseConfig,
+  shouldUseFirebaseEmulators,
+} from './firebaseConfig.js';
+export {
+  InfrastructureError,
+  createInfrastructureError,
+  normalizeFirebaseError,
+} from './firebaseErrors.js';
+export { getFirebaseApp } from './firebaseApp.js';
+export { getFirestoreClient } from './firestoreClient.js';
