a01b7f98393ec65e5b844591d399e94434e11ba1
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-21 16:17:36 +0700

    test(t7): point E2E permissions at isolated app origin

--- e2e/t7-mvp.spec.js
+++ e2e/t7-mvp.spec.js
@@ -2,7 +2,7 @@ import AxeBuilder from '@axe-core/playwright';
 import { expect, test } from '@playwright/test';
 
 const PROJECT_ID = 'demo-nocreport';
-const APP_ORIGIN = 'http://127.0.0.1:5174';
+const APP_ORIGIN = 'http://127.0.0.1:5187';
 const AUTH_ORIGIN = 'http://127.0.0.1:9099';
 const FIRESTORE_ORIGIN = 'http://127.0.0.1:8080';
 const PASSWORD = 'NocReport-T7-E2E-2026!';
