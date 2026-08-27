756dbded436633a4afc0f6317201ed4c71884793
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-28 02:57:03 +0700

    style: load clean Generator hierarchy overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './app/App.jsx';
 import './styles/app.css';
+import './styles/generator-clean.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
