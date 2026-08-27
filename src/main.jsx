b5fbbcbb44acb7abcd49df1219289ef1224433f7
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-28 03:06:38 +0700

    style: load Generator polish pass

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App.jsx';
 import './styles/app.css';
 import './styles/generator-clean.css';
+import './styles/generator-polish.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
