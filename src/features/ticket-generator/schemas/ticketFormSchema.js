0db4ea289fa878f4c70f5927678cd83f1b54720c
jangangitungapah-pixel <jangangitungapah@gmail.com>
2026-08-28 19:43:43 +0700

    fix: default optional Closed Time in form schema

--- src/features/ticket-generator/schemas/ticketFormSchema.js
+++ src/features/ticket-generator/schemas/ticketFormSchema.js
@@ -8,7 +8,7 @@ export const ticketFormSchema = z
     impactList: z.array(z.object({ value: z.string() })),
     occurAt: z.string(),
     dispatchAt: z.string(),
-    closedAt: z.string(),
+    closedAt: z.string().default(''),
     pic: z.string(),
     rootcause: z.string(),
     cutPoint: z.string(),
